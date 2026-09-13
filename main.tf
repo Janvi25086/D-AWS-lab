837b825ff9e56bd0d9bae75185d202c9cff0f328
Janvi25086 <janviii1505@gmail.com>
2026-09-13 20:27:09 +0530

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket_prefix= "d-aws-lab"
+}
